9919e04f3791fee58068f6fb8ebd5750089f79d1
pandemicGander <dannygonline@gmail.com>
2014-06-24 14:35:02 +0100

    Cockpit trail complete

---
+++ cuppytrailcockpit/gensrc/de/hybris/platform/cuppytrailcockpit/constants/GeneratedCuppytrailcockpitConstants.java
@@ -0,0 +1,35 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 24-Jun-2014 12:57:26                        ---
+ * ----------------------------------------------------------------
+ *  
+ * [y] hybris Platform
+ *  
+ * Copyright (c) 2000-2011 hybris AG
+ * All rights reserved.
+ *  
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ *  
+ */
+package de.hybris.platform.cuppytrailcockpit.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedCuppytrailcockpitConstants
+{
+	public static final String EXTENSIONNAME = "cuppytrailcockpit";
+	
+	protected GeneratedCuppytrailcockpitConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ cuppytrailcockpit/web/webroot/index.jsp
@@ -0,0 +1,7 @@
+<html>
+	<body>
+		<h1>cuppytrailcockpit</h1>
+		Welcome to my extension.
+	</body>
+</html>
+
